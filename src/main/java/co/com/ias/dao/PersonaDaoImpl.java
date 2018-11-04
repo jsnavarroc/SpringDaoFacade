526910063558727c58db2aec1c659775910a015f
Johan Navarro Cano <21sebas12@gmail.com>
2018-11-04 18:36:21 -0500

    Se modifica el PersonaDaoImpl para utilziar SpringJDBCTemplate

--- src/main/java/co/com/ias/dao/PersonaDaoImpl.java
+++ src/main/java/co/com/ias/dao/PersonaDaoImpl.java
@@ -7,34 +7,23 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import co.com.ias.model.Persona;
 
 @Repository
-public class PersonaDaoImpl implements IPersonaDao {
+public class PersonaDaoImpl extends JdbcDaoSupport implements IPersonaDao {
+
 	@Autowired
-	private DataSource dataSource;
+	public PersonaDaoImpl(DataSource dataSource) {
+		setDataSource(dataSource);
+	}
 
 	@Override
 	public void create(Persona persona) {
 		String sql = "INSERT INTO PERSONA (NOMBRES, APELLIDOS) VALUES (?, ?)";
-
-		try (Connection cx = dataSource.getConnection()) {
-			PreparedStatement ps = cx.prepareStatement(sql);
-			ps.setString(1, persona.getNombres());
-			ps.setString(2, persona.getApellidos());
-			ps.executeUpdate();
-			ps.close();
-			System.out.println("--Persona Creada--");
-			System.out.println(persona.getId());
-			System.out.println(persona.getNombres());
-			System.out.println(persona.getApellidos());
-		} catch (Exception e) {
-			throw new RuntimeException(e+"\n ##Falla en la conccion##");
-		}
-
-
+		getJdbcTemplate().update(sql, new Object[] {persona.getNombres(),persona.getApellidos()});
 	}
 
 	@Override
