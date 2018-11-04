b13ac1b73d3f9d6fc7c0df7b58998964611eb758
Johan Navarro Cano <21sebas12@gmail.com>
2018-11-04 17:05:51 -0500

    Se establece la coneccion  con DataSurce

--- src/main/java/co/com/ias/dao/PersonaDaoImpl.java
+++ src/main/java/co/com/ias/dao/PersonaDaoImpl.java
@@ -1,26 +1,46 @@
 package co.com.ias.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import co.com.ias.model.Persona;
 
 @Repository
 public class PersonaDaoImpl implements IPersonaDao {
+	@Autowired
+	private DataSource dataSource;
 
 	@Override
 	public void create(Persona persona) {
-		System.out.println("--Persona Creada--");
-		System.out.println(persona.getId());
-		System.out.println(persona.getNombres());
-		System.out.println(persona.getApellidos());
+		String sql = "INSERT INTO PERSONA (NOMBRES, APELLIDOS) VALUES (?, ?)";
+
+		try (Connection cx = dataSource.getConnection()) {
+			PreparedStatement ps = cx.prepareStatement(sql);
+			ps.setString(1, persona.getNombres());
+			ps.setString(2, persona.getApellidos());
+			ps.executeUpdate();
+			ps.close();
+			System.out.println("--Persona Creada--");
+			System.out.println(persona.getId());
+			System.out.println(persona.getNombres());
+			System.out.println(persona.getApellidos());
+		} catch (Exception e) {
+			throw new RuntimeException(e+"\n ##Falla en la conccion##");
+		}
+
+
 	}
 
 	@Override
 	public void update(Persona persona) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -38,7 +58,7 @@ public class PersonaDaoImpl implements IPersonaDao {
 	@Override
 	public void delete(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
